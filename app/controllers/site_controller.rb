3983030cacef7d0f14e03a202e78041739168b3f
tonyennis145 <ennis.tony@gmail.com>
2020-03-29 15:05:44 +0800

    Allow i-framing demo site

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,12 @@
 class SiteController < ApplicationController
 
+  before_filter :allow_iframe
+
   def home
+  end
+
+  def allow_iframe
+    response.headers.delete 'X-Frame-Options'
   end
 
 end
