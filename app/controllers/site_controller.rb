d9914ea84133e1d5ac53662af1b95e9c236ded3a
tonyennis145 <ennis.tony@gmail.com>
2020-05-22 11:05:52 +0800

    Add basecamp

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,8 +5,31 @@ class SiteController < ApplicationController
   def home
   end
 
+  def bc
+  end
+
+  def page
+    begin
+      if page_is_whitelisted?
+    	render params[:page].to_sym, :layout => false
+      end
+    rescue => errors
+      Rails.logger.info("e: #{errors}")
+    end
+  end
+
   def allow_iframe
     response.headers.delete 'X-Frame-Options'
+  end
+
+  private
+
+  def page_is_whitelisted?
+    true if whitelisted_pages.include?(params[:page])
+  end
+
+  def whitelisted_pages
+    %w(home bc)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  
   root :to => "site#home"
+  get "/:page" => "site#page", :as => "page_path"
+
+  get "/chunksapp/clear_cache" => "site#home" , :as => :chunks_clear_cache
+
 end
